37a920caa7ab20bb9b16ffd5ca9af950600d5e73
Tyler Long <tyler4long@gmail.com>
2020-08-03 14:14:28 -0700

    Try Paco's code

--- src/index.ts
+++ src/index.ts
@@ -13,4 +13,23 @@ rc.token = {access_token: process.env.RINGCENTRAL_ACCESS_TOKEN};
     .extension(process.env.RINGCENTRAL_EXTENSION_ID)
     .get();
   console.log(JSON.stringify(ext, null, 2));
+
+  await rc
+    .restapi()
+    .account()
+    .extension(process.env.RINGCENTRAL_EXTENSION_ID)
+    .put({
+      status: 'Enabled',
+      extensionNumber: '222', // leave this or make sure the number are not being used
+      contact: {
+        firstName: 'Demo',
+        lastName: 'Automated',
+        company: 'Test',
+        jobTitle: 'consultant',
+        email: 'valid.email@address.com',
+        emailAsLoginName: true,
+      },
+      password: 'MyAwes0mePassw0rd',
+    });
+  console.log('done');
 })();
