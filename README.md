b95ecd84e8d33c0e4ae1ddbbdeec9c0ac6cb4e3c
Tyler Long <tyler4long@gmail.com>
2020-08-03 13:24:36 -0700

    Get ext info

---
+++ README.md
@@ -0,0 +1,15 @@
+# Update RingCentral extension demo
+
+## Setup
+
+copy `.env.sample` as `.env`, edit it to specify credentials.
+
+```
+yarn install
+```
+
+## Run
+
+```
+yarn test
+```
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('Hello world');
+import RingCentral from '@rc-ex/core';
+
+const rc = new RingCentral({
+  server: process.env.RINGCENTRAL_SERVER_URL,
+});
+
+rc.token = {access_token: process.env.RINGCENTRAL_ACCESS_TOKEN};
+
+(async () => {
+  const ext = await rc
+    .restapi()
+    .account()
+    .extension(process.env.RINGCENTRAL_EXTENSION_ID)
+    .get();
+  console.log(JSON.stringify(ext, null, 2));
+})();
