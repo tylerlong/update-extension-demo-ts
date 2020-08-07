3a805f6bee6ab19d5d232e7cb3b29988852eb9a6
Tyler Long <tyler4long@gmail.com>
2020-08-06 18:10:47 -0700

    First working version

--- README.md
+++ README.md
@@ -13,3 +13,11 @@ yarn install
 ```
 yarn test
 ```
+
+## Conclusion
+
+For SSO enabled customers, you cannot specify password because password is not allowed. "Credentials changing is not allowed".
+
+You should specify "NotActivated" instead of "Enabled", otherwise API will complain "parameter password invalid".
+
+I don't think it is an ideal solution. It is a workaround instead.
--- src/index.ts
+++ src/index.ts
@@ -19,17 +19,17 @@ rc.token = {access_token: process.env.RINGCENTRAL_ACCESS_TOKEN};
     .account()
     .extension(process.env.RINGCENTRAL_EXTENSION_ID)
     .put({
-      status: 'Enabled',
-      extensionNumber: '222', // leave this or make sure the number are not being used
+      status: 'NotActivated', // do not specify 'Enabled'
+      extensionNumber: '11888', // leave this or make sure the number are not being used
       contact: {
         firstName: 'Demo',
         lastName: 'Automated',
         company: 'Test',
         jobTitle: 'consultant',
-        email: 'valid.email.uniq.3421896@address.com',
+        email: 'valid.email.uniq.323232@address.com', // must be unique
         emailAsLoginName: true,
       },
-      password: 'MyAwes0mePassw0rd',
+      // password: 'MyAwes0mePassw0rd', // do not specify password
     });
   console.log('done');
 })();
